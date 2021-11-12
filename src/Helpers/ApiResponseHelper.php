bcf0a8841cedb07f7a711a96e2cbf080006bb6d6
abbasnaumani <abbasnaumani@gmail.com>
2021-11-12 19:09:00 +0500

    data replaces with payload in response

--- src/Helpers/ApiResponseHelper.php
+++ src/Helpers/ApiResponseHelper.php
@@ -18,61 +18,111 @@ class ApiResponseHelper
         $this->responseFormat = config('apiresponse.response_format');
     }
 
+    /**
+     * Method to Set Status
+     */
     public function setStatus($status)
     {
         $this->status = $status;
     }
 
+    /**
+     * Method to Get Status
+     *
+     * @return mixed
+     */
     public function getStatus()
     {
         return $this->status;
     }
 
+    /**
+     * Method to Set Code
+     */
     public function setCode($code)
     {
         $this->code = $code;
     }
 
+    /**
+     * Method to Get Code
+     *
+     * @return mixed
+     */
     public function getCode()
     {
         return $this->code;
     }
 
+    /**
+     * Method to Set Message
+     */
     public function setMessage($message)
     {
         $this->message = $message;
     }
 
+    /**
+     * Method to Get Message
+     *
+     * @return mixed
+     */
     public function getMessage()
     {
         return $this->message;
     }
 
+    /**
+     * Method to Set Payload
+     */
     public function setPayload($payload)
     {
         $this->payload = $payload;
     }
 
+    /**
+     * Method to Get Payload
+     *
+     * @return mixed
+     */
     public function getPayload()
     {
         return $this->payload;
     }
 
+    /**
+     * Method to Set Response Format
+     */
     public function setResponseFormat($responseFormat)
     {
         $this->responseFormat = $responseFormat;
     }
 
+    /**
+     * Method to Get Response Format
+     *
+     * @return mixed
+     */
     public function getResponseFormat()
     {
         return $this->responseFormat;
     }
 
+    /**
+     * Method to Get Response
+     *
+     * @return mixed
+     */
     public function getResponse()
     {
         return $this;
     }
 
+    /**
+     * Method to Get API Response
+     *
+     * @return mixed
+     */
     public function getApiResponse()
     {
         if ($this->responseFormat == config('apiresponse.response_format')) {
--- src/Traits/ApiResponseTrait.php
+++ src/Traits/ApiResponseTrait.php
@@ -6,7 +6,9 @@ use Kodestudio\ApiResponse\Facades\ApiResponseFacade;
 
 trait ApiResponseTrait
 {
-
+    /**
+     * Method to Set Api Success Message
+     */
     public function setApiSuccessMessage($msg, $payload = null, $statusCode = 200)
     {
         ApiResponseFacade::setStatus(config('apiresponse.success_response'));
@@ -15,6 +17,9 @@ trait ApiResponseTrait
         ApiResponseFacade::setCode($statusCode);
     }
 
+    /**
+     * Method to Set Api Error Message
+     */
     public function setApiErrorMessage($msg, $payload = null, $statusCode = 400)
     {
         ApiResponseFacade::setMessage($msg);
@@ -22,21 +27,41 @@ trait ApiResponseTrait
         ApiResponseFacade::setCode(401);
     }
 
+    /**
+     * Method to Get Code
+     *
+     * @return mixed
+     */
     public function getCode()
     {
         return ApiResponseFacade::getCode();
     }
 
+    /**
+     * Method to Get Response
+     *
+     * @return mixed
+     */
     public function getResponse()
     {
         return ApiResponseFacade::getResponse();
     }
 
+    /**
+     * Method to Get Api Response
+     *
+     * @return mixed
+     */
     public function getApiResponse()
     {
         return ApiResponseFacade::getApiResponse();
     }
 
+    /**
+     * Method to Get Api Error Message
+     *
+     * @return mixed
+     */
     private function extractErrorMessage($exceptionErrors)
     {
         if (isset($exceptionErrors) && is_array($exceptionErrors) && count($exceptionErrors) > 0) {
@@ -46,5 +71,22 @@ trait ApiResponseTrait
         } else {
             return $exceptionErrors;
         }
+    }
+
+    /**
+     * Method to Get Api Response
+     * @param $e
+     * @return array
+     */
+    public function traceErrors($e): array
+    {
+        return [
+            'getPrevious' => $e->getPrevious(),
+            'getMessage' => $e->getMessage(),
+            'getCode' => $e->getCode(),
+            'getFile' => $e->getFile(),
+            'getLine' => $e->getLine(),
+            'getTrace' => $e->getTrace()
+        ];
     }
 }
